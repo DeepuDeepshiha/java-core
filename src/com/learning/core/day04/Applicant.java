c9089dc0ff8dc98e04831b7b7819762f88d53efb
DeepuDeepshiha <161846915+DeepuDeepshiha@users.noreply.github.com>
2024-03-08 08:10:14 +0530

    Add files via upload

---
+++ src/com/learning/core/day04/Applicant.java
@@ -0,0 +1,13 @@
+package com.learning.core.day04;
+
+public class Applicant {
+	String applicantname;
+	String postapplied;
+	int applicantage;
+	public Applicant(String name, String post, int age)
+	{
+		applicantname=name;
+		postapplied=post;
+		applicantage=age;
+	}
+}
