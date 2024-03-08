7f1e4ae4fb2b2bb6e8dcb7d2578c54d82777e594
DeepuDeepshiha <161846915+DeepuDeepshiha@users.noreply.github.com>
2024-03-08 08:16:17 +0530

    Add files via upload

---
+++ src/com/learning/core/day05/D05P04.java
@@ -0,0 +1,27 @@
+package com.learning.core.day05;
+
+import java.util.HashSet;
+
+public class D05P04 {
+    public static void main(String[] args) {
+        HashSet<Product> productSet = new HashSet<Product>();
+
+        productSet.add(new Product("P001", "Maruthi 800"));
+        productSet.add(new Product("P002", "MaruthiZen"));
+        productSet.add(new Product("P003", "Maruti Dezire"));
+        productSet.add(new Product("P004", "Maruthi Alto"));
+
+        String searchProductId = "P003";
+        String searchProductName = "Maruti Dezire";
+        Product searchProduct = new Product(searchProductId, searchProductName);
+
+        if (productSet.contains(searchProduct)) 
+        {
+            System.out.println("Product Found");
+        } 
+        else 
+        {
+            System.out.println("Product Not Found");
+        }
+    }
+}
