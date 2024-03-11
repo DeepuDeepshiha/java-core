28308707ddc5ddaa3c123730dfbaf027fc84eff0
DeepuDeepshiha <161846915+DeepuDeepshiha@users.noreply.github.com>
2024-03-11 21:11:40 +0530

    Add files via upload

---
+++ src/com/learning/core/Day4/InvalidAgeException.java
@@ -0,0 +1,9 @@
+package com.learning.core.day4;
+
+public class InvalidAgeException extends Exception{
+	public InvalidAgeException()
+	{
+		System.out.println("Invalid age");
+	}
+
+}
---
+++ src/com/learning/core/Day4/depositexception.java
@@ -0,0 +1,9 @@
+package com.learning.core.day4;
+
+public class depositexception extends Exception {
+	public depositexception()
+	{
+		System.out.println("Negative amount");
+	}
+
+}
