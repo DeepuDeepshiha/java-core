6d03a33399034488d1fb80cd369370bab77a0dca
DeepuDeepshiha <161846915+DeepuDeepshiha@users.noreply.github.com>
2024-03-11 21:19:13 +0530

    Add files via upload

---
+++ src/com/learning/core/Day5/D05P03.java
@@ -0,0 +1,20 @@
+package com.learning.core.day5;
+import java.util.HashSet;
+
+public class D05P03 {
+    public static void main(String[] args) {
+        
+        HashSet<Product> productSet = new HashSet<Product>();
+
+        productSet.add(new Product("P001", "Maruthi 800"));
+        productSet.add(new Product("P002", "MaruthiZen"));
+        productSet.add(new Product("P003", "Maruti Dezire"));
+        productSet.add(new Product("P004", "Maruthi Alto"));
+
+      
+        for (Product product : productSet)
+        {
+            System.out.println(product.productId+" "+product.productName);
+        }
+    }
+}
