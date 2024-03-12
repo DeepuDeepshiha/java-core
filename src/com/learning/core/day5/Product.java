a2b24e637d2bce8cfbaca902319553270f838d23
DeepuDeepshiha <161846915+DeepuDeepshiha@users.noreply.github.com>
2024-03-12 19:45:34 +0530

    Add files via upload

---
+++ src/com/learning/core/day5/Product.java
@@ -0,0 +1,40 @@
+package com.learning.core.day5;
+
+public class Product {
+    public String productId;
+    public String productName;
+
+    public Product(String productId, String productName)
+    {
+        this.productId = productId;
+        this.productName = productName;
+    }
+
+    public String getProductId() 
+    {
+        return productId;
+    }
+
+    public String getProductName() 
+    {
+        return productName;
+    }
+
+    
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Product product = (Product) obj;
+        return productId.equals(product.productId);
+    }
+
+    
+    public int hashCode()
+    {
+        return productId.hashCode();
+    }
+}
+
