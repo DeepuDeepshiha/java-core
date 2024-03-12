8911688e055bd2ef21f64322736369ef5674c8fb
DeepuDeepshiha <161846915+DeepuDeepshiha@users.noreply.github.com>
2024-03-12 08:56:11 +0530

    Update D06P09.java

--- src/com/learning/core/Day6/D06P09.java
+++ src/com/learning/core/Day6/D06P09.java
@@ -6,28 +6,23 @@ import java.util.TreeMap;
 class D06P09{ 
 
 	public static void main(String[] args) {
-		TreeMap<Car1, String> tr = new TreeMap<>(); 
-		tr.put(new Car1("Bugatti", 80050.0), "1"); 
-		tr.put(new Car1("Swift", 305000.0), "2"); 
-		tr.put(new Car1("Audi", 600100.0), "3"); 
-		tr.put(new Car1("Benz", 900000.0), "4"); 
+		TreeMap<Car, String> tr = new TreeMap<>(); 
+		tr.put(new Car("Bugatti", 80050.0), "1"); 
+		tr.put(new Car("Swift", 305000.0), "2"); 
+		tr.put(new Car("Audi", 600100.0), "3"); 
+		tr.put(new Car("Benz", 900000.0), "4"); 
 		double keyToReplace = 80050.0; 
 		String newValue = "Reva"; 
 		for (Map.Entry<Car1, String> entry : tr.entrySet()) 
 		{ 
 			if (entry.getKey().price == keyToReplace) 
 			{ 
-
 				entry.setValue(newValue);
-
 				break;
-
-				} 
-
 			} 
-		System.out.println("Updated Car Details:");
 
-		for (Map.Entry<Car1, String> entry : tr.entrySet()) 
+		} 
+		for (Map.Entry<Car, String> entry : tr.entrySet()) 
 		{ 
 			System.out.println(entry. getValue()+". "+entry.getKey());
 
