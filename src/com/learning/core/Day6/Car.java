6af5823b1223e879ce2f2c31479a7772bba7aafe
DeepuDeepshiha <161846915+DeepuDeepshiha@users.noreply.github.com>
2024-03-12 10:33:59 +0530

    Update Car.java

--- src/com/learning/core/Day6/Car.java
+++ src/com/learning/core/Day6/Car.java
@@ -2,8 +2,24 @@ package com.learning.core.day6;
 import java.util.*;
 
 class Car implements Comparable<Car> {
-    private String name;
-    private double price;
+     public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	String name;
+    double price;
 
     public Car(String name, double price) {
         this.name = name;
